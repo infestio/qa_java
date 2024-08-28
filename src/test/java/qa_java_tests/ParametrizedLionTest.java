49d9de122dd7b9ade2af12bc7d3f0d5cb5cb6d96
Roman Buryak <infestberserk@gmail.com>
2024-08-28 13:59:47 +0400

    add jacoco report

---
+++ src/test/java/qa_java_tests/ParametrizedLionTest.java
@@ -0,0 +1,35 @@
+package qa_java_tests;
+
+import com.example.Lion;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+@RunWith(Parameterized.class)
+public class ParametrizedLionTest {
+
+    private final String sexLion;
+    private final boolean expectedHasMane;
+
+    public ParametrizedLionTest(String sexLion, boolean expectedHasMane) {
+        this.sexLion = sexLion;
+        this.expectedHasMane = expectedHasMane;
+    }
+
+    @Parameterized.Parameters
+    public static Object[][] getCountKittens() {
+        return new Object[][] {
+                {"Самец",true},
+                {"Самка",false},
+        };
+    }
+
+    @Test
+    public void doesHaveManeOrNotLionDoneTest() throws Exception {
+        Lion lion = new Lion(sexLion);
+        boolean actualResult = lion.doesHaveMane();
+        Assert.assertEquals(expectedHasMane, actualResult);
+
+    }
+}
